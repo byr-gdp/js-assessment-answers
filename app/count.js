f681f5518cbce16f94a830c594ccd8224bedd3a5
byr-gdp <anthozoan77@gmail.com>
2016-03-09 15:11:08 +0800

    finish count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    // output the first num immediately
+    console.log(start++);
+    var s = setInterval(function() {
+        if(start <= end) {
+            console.log(start++);
+        } else {
+            clearInterval(s);
+        }
+    }, 100);
+    var o = {
+      cancel: function() {
+        clearInterval(s);
+      }
+    };
 
+    return o;
   }
 };
